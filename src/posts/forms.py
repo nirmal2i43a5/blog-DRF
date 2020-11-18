90ba2e44ad449b49be77ff590b9d6745447d7ff9
Nirmal <nirmalpandey048@gmail.com>
2020-11-18 16:43:30 +0545

    added previous 'blog project to make its api

---
+++ src/posts/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+
+        
+from ckeditor_uploader.widgets import CKEditorUploadingWidget
+
+
+from .models import Post
+
+
+class PostForm(forms.ModelForm):
+    content = forms.CharField(widget=CKEditorUploadingWidget())
+    publish = forms.DateField(widget=forms.SelectDateWidget)
+    class Meta:
+        model = Post
+        fields = [
+            "title",
+            "content",
+            "image",
+            "draft",
+            "publish",
+        ]
+
+
+  
---
+++ src/posts/models.py
@@ -0,0 +1,109 @@
+from __future__ import unicode_literals
+
+from django.conf import settings
+from django.contrib.contenttypes.models import ContentType
+from django.core.urlresolvers import reverse
+from django.db import models
+from django.db.models.signals import pre_save
+from django.utils import timezone
+from django.utils.safestring import mark_safe
+from django.utils.text import slugify
+
+
+from markdown_deux import markdown
+from comments.models import Comment
+
+from .utils import get_read_time
+
+from ckeditor.fields import RichTextField
+from ckeditor_uploader.fields import RichTextUploadingField
+
+
+
+class PostManager(models.Manager):
+    def active(self, *args, **kwargs):
+        # Post.objects.all() = super(PostManager, self).all()
+        return super(PostManager, self).filter(draft=False).filter(publish__lte=timezone.now())
+
+
+class Post(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, default=1)
+    title = models.CharField(max_length=120)
+    slug = models.SlugField(unique=True)
+    image = models.ImageField(upload_to='Images', 
+            null=True, 
+            blank=True, 
+            width_field="width_field", 
+            height_field="height_field")
+    height_field = models.IntegerField(default=0)
+    width_field = models.IntegerField(default=0)
+    
+    content = RichTextUploadingField(null=True,blank=True)
+    
+    draft = models.BooleanField(default=False)
+    publish = models.DateField(auto_now=False, auto_now_add=False)
+    read_time =  models.IntegerField(default=0) # models.TimeField(null=True, blank=True) #assume minutes
+    updated = models.DateTimeField(auto_now=True, auto_now_add=False)
+    timestamp = models.DateTimeField(auto_now=False, auto_now_add=True)
+
+    objects = PostManager()
+
+    def __unicode__(self):
+        return self.title
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("posts:detail", kwargs={"slug": self.slug})
+
+    class Meta:
+        ordering = ["-timestamp", "-updated"]
+
+
+    @property
+    def comments(self):
+        instance = self
+        qs = Comment.objects.filter_by_instance(instance)
+        return qs
+
+    @property
+    def get_content_type(self):
+        instance = self
+        content_type = ContentType.objects.get_for_model(instance.__class__)
+        return content_type
+
+
+def create_slug(instance, new_slug=None):
+    slug = slugify(instance.title)
+    if new_slug is not None:
+        slug = new_slug
+    qs = Post.objects.filter(slug=slug).order_by("-id")
+    exists = qs.exists()
+    if exists:
+        new_slug = "%s-%s" %(slug, qs.first().id)
+        return create_slug(instance, new_slug=new_slug)
+    return slug
+
+
+def pre_save_post_receiver(sender, instance, *args, **kwargs):
+    if not instance.slug:
+        instance.slug = create_slug(instance)
+
+    if instance.content:
+        html_string = instance.get_markdown()
+        read_time_var = get_read_time(html_string)
+        instance.read_time = read_time_var
+
+
+pre_save.connect(pre_save_post_receiver, sender=Post)
+
+
+
+
+
+
+
+
+
+
