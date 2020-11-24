f76b7b34cd5c86911b9ab479c6a13b34e925cb83
Nirmal <nirmalpandey048@gmail.com>
2020-11-24 10:15:42 +0545

    added create_model to work with comment

--- src/comments/models.py
+++ src/comments/models.py
@@ -20,7 +20,31 @@ class CommentManager(models.Manager):
         qs = super(CommentManager, self).filter(content_type=content_type, object_id= obj_id).filter(parent=None)#parent is used for reply logic
         #super(CommentManager, self) means Comment.objects
         return qs
+    
+    #this is for comment serializers
+    def create_by_model_type(self, model_type, slug, content, user, parent_obj=None):
+        model_qs = ContentType.objects.filter(model=model_type)
+        if model_qs.exists():
+            SomeModel = model_qs.first().model_class()
+            obj_qs = SomeModel.objects.filter(slug=slug)
+            if obj_qs.exists() and obj_qs.count() == 1:
+                instance = self.model()
+                instance.content = content
+                instance.user = user
+                instance.content_type = model_qs.first()
+                instance.object_id = obj_qs.first().id
+                if parent_obj:
+                    instance.parent = parent_obj
+                instance.save()
+                return instance
+        return None
 
+    
+      #this logic is for rest api
+      
+
+
+      
 class Comment(models.Model):
     user        = models.ForeignKey(settings.AUTH_USER_MODEL, default=1, on_delete=models.CASCADE)
     
